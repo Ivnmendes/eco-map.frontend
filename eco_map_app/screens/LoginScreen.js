c572211bf2ac74926d7412a97d916d4198538af1
Ivan <ivanmendes2005@gmail.com>
2025-06-08 15:54:30 -0300

    Melhorias visuais e de validação  nas telas de login e registro

--- eco_map_app/screens/LoginScreen.js
+++ eco_map_app/screens/LoginScreen.js
@@ -1,9 +1,22 @@
-import React, { useState, useContext } from 'react';
-import { View, TextInput, TouchableOpacity, Text, StyleSheet } from 'react-native';
+import React, { useState, useContext, useRef } from 'react';
+import {
+    View,
+    TextInput,
+    TouchableOpacity,
+    Text,
+    StyleSheet,
+    Image,
+    Animated,
+    Keyboard,
+    KeyboardAvoidingView,
+    Platform,
+    ScrollView,
+    ActivityIndicator
+} from 'react-native';
 
 import { login } from '../services/api';
-
 import { NotificationContext } from '../context/NotificationContext';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function LoginScreen({ navigation }) {
     const [email, setEmail] = useState('');
@@ -12,24 +25,46 @@ export default function LoginScreen({ navigation }) {
     const [errors, setErrors] = useState({
         email: false,
         password: false,
-        confirmPassword: false
-    })
+    });
     const { showNotification } = useContext(NotificationContext);
-
     const [showPassword, setShowPassword] = useState(false);
-    
+
+    const shakeAnimationEmail = useRef(new Animated.Value(0)).current;
+    const shakeAnimationPassword = useRef(new Animated.Value(0)).current;
+
+    const triggerShake = (animationValue) => {
+        animationValue.setValue(0);
+        Animated.sequence([
+            Animated.timing(animationValue, { toValue: 10, duration: 80, useNativeDriver: true }),
+            Animated.timing(animationValue, { toValue: -10, duration: 80, useNativeDriver: true }),
+            Animated.timing(animationValue, { toValue: 10, duration: 80, useNativeDriver: true }),
+            Animated.timing(animationValue, { toValue: 0, duration: 80, useNativeDriver: true }),
+        ]).start();
+    };
+
     function validateFields() {
+        const isEmailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+        const isPasswordValid = password.length >= 6;
+
         const newErrors = {
-            email: !email.trim() || !email.includes('@'),
-            password: !password
+            email: !isEmailValid,
+            password: !isPasswordValid,
         };
 
         setErrors(newErrors);
 
-        return !Object.values(newErrors).some(Boolean);
+        if (!isEmailValid) {
+            triggerShake(shakeAnimationEmail);
+        }
+        if (!isPasswordValid) {
+            triggerShake(shakeAnimationPassword);
+        }
+
+        return isEmailValid && isPasswordValid;
     }
 
     async function handleLogin() {
+        Keyboard.dismiss(); 
         if (validateFields()) {
             setLoading(true);
             try {
@@ -44,82 +79,164 @@ export default function LoginScreen({ navigation }) {
     }
 
     return (
-        <View style={styles.container}>
-            <View style={styles.labelContainer}>
-                <Text style={styles.label}>Email</Text>
-            </View>
-            <TextInput
-                placeholder="Email"
-                value={email}
-                onChangeText={setEmail}
-                style={[styles.input, errors.email && styles.inputError]}
-                keyboardType="email-address"
-                autoCapitalize="none"
-            />
-            <View style={styles.labelContainer}>
-                <Text style={styles.label}>Senha</Text>
-            </View>
-            <TextInput
-                placeholder="Senha"
-                value={password}
-                onChangeText={setPassword}
-                style={[styles.input, styles.passwordInput, errors.password && styles.inputError]}
-                secureTextEntry={!showPassword}
-            />
-            <TouchableOpacity
-                onPress={() => setShowPassword(!showPassword)}
-                style={styles.showButton}
-            ></TouchableOpacity>
-            <TouchableOpacity onPress={handleLogin} disabled={loading} style={styles.customButton}>
-                <Text style={styles.buttonText}>
-                    {loading ? 'Carregando...' : 'Entrar'}
-                </Text>
-            </TouchableOpacity>
-            <Text>
-                Ainda não tem uma conta? Crie uma
-            </Text>
-            <TouchableOpacity onPress={() => navigation.replace('Register')} disabled={loading} style={styles.customButton}>
-                <Text style={styles.buttonText}>
-                    Crie sua conta
-                </Text>
-            </TouchableOpacity>
-        </View>
+        <KeyboardAvoidingView 
+            behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+            style={styles.keyboardView}
+            keyboardVerticalOffset={Platform.OS === 'ios' ? 64 : 0}
+        >
+            <ScrollView contentContainerStyle={styles.container} keyboardShouldPersistTaps="handled">
+                <Image source={require('../assets/logo.png')} style={styles.image} />
+                <View style={styles.labelContainer}>
+                    <Text style={styles.label}>Email</Text>
+                </View>
+                <Animated.View style={[{ transform: [{ translateX: shakeAnimationEmail }] }, styles.inputContainer]}>
+                    <TextInput
+                        placeholder="seuemail@exemplo.com"
+                        value={email}
+                        onChangeText={setEmail}
+                        style={[styles.input, errors.email && styles.inputError]}
+                        keyboardType="email-address"
+                        autoCapitalize="none"
+                    />
+                </Animated.View>
+
+                <View style={styles.labelContainer}>
+                    <Text style={styles.label}>Senha</Text>
+                    {errors.password && <Text style={styles.errorText}> (mínimo 6 caracteres)</Text>}
+                </View>
+                <Animated.View style={styles.passwordContainer}>
+                    <Animated.View style={[{ flex: 1 }, { transform: [{ translateX: shakeAnimationPassword }] }]}>
+                        <TextInput
+                            placeholder="Senha"
+                            value={password}
+                            onChangeText={setPassword}
+                            style={[styles.input, styles.passwordInput, errors.password && styles.inputError]}
+                            secureTextEntry={!showPassword}
+                        />
+                    </Animated.View>
+                    <TouchableOpacity
+                        onPress={() => setShowPassword(!showPassword)}
+                        style={styles.showButton}
+                    >
+                        <Ionicons name={!showPassword ? 'eye-outline' : 'eye-off-outline'} size={24} color={'#256D5B'}/>
+                    </TouchableOpacity>
+                </Animated.View>
+
+                <TouchableOpacity onPress={handleLogin} disabled={loading} style={styles.customButton}>
+                    {loading ? (
+                        <ActivityIndicator color="#fff" />
+                    ) : (
+                        <Text style={styles.buttonText}>Entrar</Text>
+                    )}
+                </TouchableOpacity>
+
+                <View style={styles.registerContainer}>
+                    <Text style={styles.registerText}>
+                        Ainda não tem uma conta?
+                    </Text>
+                    <TouchableOpacity onPress={() => navigation.replace('Register')} disabled={loading}>
+                        <Text style={styles.registerButtonText}>
+                            Crie sua conta
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            </ScrollView>
+        </KeyboardAvoidingView>
     );
 }
 
 const styles = StyleSheet.create({
-    container: { 
-        flex: 1, 
+    image: {
+        width: 250,
+        height: 250,
+        alignSelf: 'center',
+        marginBottom: 20,
+        borderRadius: 20,
+    },
+    keyboardView: {
+        flex: 1,
+        backgroundColor: '#F5F5F5',
+    },
+    container: {
+        flexGrow: 1,
         alignItems: 'center',
-        justifyContent: 'center', 
-        padding: 16 
+        paddingTop: '30%',
+        paddingHorizontal: 24,
     },
     labelContainer: {
-        alignSelf: 'flex-start',
-        marginLeft: '55'
+        width: '90%',
+        flexDirection: 'row',
+        alignItems: 'flex-end',
+        marginBottom: 5,
+    },
+    label: {
+        fontWeight: 'bold',
+        color: '#333',
+    },
+    errorText: {
+        color: 'red',
+        fontSize: 12,
+    },
+    inputContainer: {
+        width: '91%',
+        marginBottom: 12,
+        borderRadius: 8,
+        overflow: 'hidden',
     },
     input: {
-        width: '75%',
+        width: '100%',
         borderWidth: 1,
-        borderColor: '#888',
+        backgroundColor: '#FFF',
+        borderColor: '#CCC',
         marginBottom: 12,
-        padding: 10,
-        borderRadius: 5,
+        paddingHorizontal: 15,
+        paddingVertical: 12,
+        borderRadius: 8,
+        fontSize: 16,
+    },
+    passwordContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        width: '91%',
+    },
+    showButton: {
+        position: 'absolute',
+        right: 15,
+        top: 14,
+        height: 30,
     },
     inputError: {
-        borderColor: 'red',
-        color: 'red'
+        borderColor: '#E53E3E', 
+        color: '#E53E3E',
     },
     customButton: {
-        width: '50%',
-        backgroundColor: '#4CAF50',
-        padding: 12,
+        width: '90%',
+        backgroundColor: '#256D5B',
+        padding: 15,
         borderRadius: 8,
         alignItems: 'center',
         marginVertical: 8,
+        elevation: 3,
+        shadowColor: '#000', 
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 2,
     },
     buttonText: {
         color: '#fff',
         fontWeight: 'bold',
+        fontSize: 16,
+    },
+    registerContainer: {
+        flexDirection: 'row',
+        marginTop: 20,
+    },
+    registerText: {
+        color: '#666',
+    },
+    registerButtonText: {
+        color: '#256D5B',
+        fontWeight: 'bold',
+        marginLeft: 5,
     },
 });
--- eco_map_app/screens/RegisterScreen.js
+++ eco_map_app/screens/RegisterScreen.js
@@ -1,8 +1,21 @@
-import React, { useState, useContext } from 'react';
-import { View, TextInput, TouchableOpacity, Text, StyleSheet } from 'react-native';
+import React, { useState, useContext, useRef } from 'react';
+import {
+    View,
+    TextInput,
+    TouchableOpacity,
+    Text,
+    StyleSheet,
+    Animated,
+    Keyboard,
+    KeyboardAvoidingView,
+    Platform,
+    ScrollView,
+    ActivityIndicator
+} from 'react-native';
 
 import { register } from '../services/api';
 import { NotificationContext } from '../context/NotificationContext';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function RegisterScreen({ navigation }) {
     const [firstName, setFirstName] = useState('');
@@ -19,25 +32,53 @@ export default function RegisterScreen({ navigation }) {
         confirmPassword: false,
     });
     const { showNotification } = useContext(NotificationContext);
-
     const [showPassword, setShowPassword] = useState(false);
     const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
+    const shakeAnimationFirstName = useRef(new Animated.Value(0)).current;
+    const shakeAnimationLastName = useRef(new Animated.Value(0)).current;
+    const shakeAnimationEmail = useRef(new Animated.Value(0)).current;
+    const shakeAnimationPassword = useRef(new Animated.Value(0)).current;
+    const shakeAnimationConfirmPassword = useRef(new Animated.Value(0)).current;
+
+    const triggerShake = (animationValue) => {
+        animationValue.setValue(0);
+        Animated.sequence([
+            Animated.timing(animationValue, { toValue: 10, duration: 80, useNativeDriver: true }),
+            Animated.timing(animationValue, { toValue: -10, duration: 80, useNativeDriver: true }),
+            Animated.timing(animationValue, { toValue: 10, duration: 80, useNativeDriver: true }),
+            Animated.timing(animationValue, { toValue: 0, duration: 80, useNativeDriver: true }),
+        ]).start();
+    };
+
     function validateFields() {
+        const isFirstNameValid = firstName.trim() !== '' && firstName.length <= 30;
+        const isLastNameValid = lastName.trim() !== '' && lastName.length <= 150;
+        const isEmailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+        const isPasswordValid = password.length >= 8 && !/^\d+$/.test(password);
+        const doPasswordsMatch = password === confirmPassword;
+
         const newErrors = {
-            firstName: !firstName.trim(),
-            lastName: !lastName.trim(),
-            email: !email.trim() || !email.includes('@'),
-            password: !password,
-            confirmPassword: !confirmPassword || confirmPassword !== password,
+            firstName: !isFirstNameValid,
+            lastName: !isLastNameValid,
+            email: !isEmailValid,
+            password: !isPasswordValid,
+            confirmPassword: !doPasswordsMatch || !confirmPassword,
         };
 
         setErrors(newErrors);
+
+        if (newErrors.firstName) triggerShake(shakeAnimationFirstName);
+        if (newErrors.lastName) triggerShake(shakeAnimationLastName);
+        if (newErrors.email) triggerShake(shakeAnimationEmail);
+        if (newErrors.password) triggerShake(shakeAnimationPassword);
+        if (newErrors.confirmPassword) triggerShake(shakeAnimationConfirmPassword);
 
         return !Object.values(newErrors).some(Boolean);
     }
 
     async function handleRegister() {
+        Keyboard.dismiss();
         if (validateFields()) {
             setLoading(true);
             try {
@@ -48,10 +89,16 @@ export default function RegisterScreen({ navigation }) {
                   password,
                   confirm_password: confirmPassword,
                 });
+                showNotification('success', 'Conta criada com sucesso!');
                 navigation.replace('Main');
             } catch (error) {
-                console.log(error.request)
-                showNotification('error', 'Registro de conta falhou. Verifique os campos.');
+                if (error.response && error.response.data && error.response.data.email) {
+                    showNotification('error', 'Este email já está cadastrado. Tente outro.');
+                    setErrors(prevErrors => ({ ...prevErrors, email: true }));
+                    triggerShake(shakeAnimationEmail);
+                } else {
+                    showNotification('error', 'Registro de conta falhou. Verifique os campos.');
+                }
             } finally {
                 setLoading(false);
             }
@@ -59,145 +106,201 @@ export default function RegisterScreen({ navigation }) {
     }
 
     return (
-        <View style={styles.container}>
-            <View style={styles.labelContainer}>
-                <Text style={styles.label}>Primeiro nome</Text>
-            </View>
-            <TextInput
-                placeholder="Primeiro nome"
-                value={firstName}
-                onChangeText={setFirstName}
-                style={[styles.input, errors.firstName && styles.inputError]}
-            />
-            <View style={styles.labelContainer}>
-                <Text style={styles.label}>Último nome</Text>
-            </View>
-            <TextInput
-                placeholder="Último nome"
-                value={lastName}
-                onChangeText={setLastName}
-                style={[styles.input, errors.lastName && styles.inputError]}
-            />
-            <View style={styles.labelContainer}>
-                <Text style={styles.label}>Email</Text>
-            </View>
-            <TextInput
-                placeholder="Email"
-                value={email}
-                onChangeText={setEmail}
-                style={[styles.input, errors.email && styles.inputError]}
-                keyboardType="email-address"
-                autoCapitalize="none"
-            />
-            <View style={styles.labelContainer}>
-                <Text style={styles.label}>Senha</Text>
-            </View>
-            <View style={styles.passwordContainer}>
-                <TextInput
-                    placeholder="Senha"
-                    value={password}
-                    onChangeText={setPassword}
-                    style={[styles.input, styles.passwordInput, errors.password && styles.inputError]}
-                    secureTextEntry={!showPassword}
-                />
-                <TouchableOpacity
-                    onPress={() => setShowPassword(!showPassword)}
-                    style={styles.showButton}
-                >
-                <Text style={styles.showButtonText}>{showPassword ? 'Ocultar' : 'Mostrar'}</Text>
-                </TouchableOpacity>
-            </View>
-            <View style={styles.labelContainer}>
-                <Text style={styles.label}>Confirme sua senha</Text>
-            </View>
-            <View style={styles.passwordContainer}>
-                <TextInput
-                    placeholder="Confirme sua senha"
-                    value={confirmPassword}
-                    onChangeText={setConfirmPassword}
-                    style={[styles.input, styles.passwordInput, errors.confirmPassword && styles.inputError]}
-                    secureTextEntry={!showConfirmPassword}
-                />
-                <TouchableOpacity
-                    onPress={() => setShowConfirmPassword(!showConfirmPassword)}
-                    style={styles.showButton}
-                >
-                <Text style={styles.showButtonText}>{showConfirmPassword ? 'Ocultar' : 'Mostrar'}</Text>
-                </TouchableOpacity>
-            </View>
-
-            <TouchableOpacity
-                    onPress={handleRegister}
-                    disabled={loading}
-                    style={styles.customButton}
+        <KeyboardAvoidingView
+            style={styles.keyboardView}
+            behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        >
+            <ScrollView
+                contentContainerStyle={styles.container}
+                keyboardShouldPersistTaps="handled"
+                showsVerticalScrollIndicator={false}
             >
-                <Text style={styles.buttonText}>{loading ? 'Carregando...' : 'Criar conta'}</Text>
-            </TouchableOpacity>
+                <Text style={styles.title}>Crie sua Conta</Text>
 
-            <Text>Já tem uma conta?</Text>
+                <View style={styles.labelContainer}>
+                    <Text style={styles.label}>Primeiro nome</Text>
+                    {errors.firstName && <Text style={styles.errorText}> (obrigatório, máx. 30 caracteres)</Text>}
+                </View>
+                <Animated.View style={[{ transform: [{ translateX: shakeAnimationFirstName }] }, styles.inputWrapper]}>
+                    <TextInput
+                        placeholder="Ex: João"
+                        value={firstName}
+                        onChangeText={setFirstName}
+                        style={[styles.input, errors.firstName && styles.inputError]}
+                    />
+                </Animated.View>
 
-            <TouchableOpacity
-                onPress={() => navigation.replace('Login')}
-                disabled={loading}
-                style={styles.customButton}
-            >
-                <Text style={styles.buttonText}>Entre na sua conta</Text>
-            </TouchableOpacity>
-        </View>
+                <View style={styles.labelContainer}>
+                    <Text style={styles.label}>Último nome</Text>
+                    {errors.lastName && <Text style={styles.errorText}> (obrigatório, máx. 150 caracteres)</Text>}
+                </View>
+                <Animated.View style={[{ transform: [{ translateX: shakeAnimationLastName }] }, styles.inputWrapper]}>
+                    <TextInput
+                        placeholder="Ex: Silva"
+                        value={lastName}
+                        onChangeText={setLastName}
+                        style={[styles.input, errors.lastName && styles.inputError]}
+                    />
+                </Animated.View>
+
+                <View style={styles.labelContainer}>
+                    <Text style={styles.label}>Email</Text>
+                    {errors.email && <Text style={styles.errorText}> (formato inválido ou já existente)</Text>}
+                </View>
+                <Animated.View style={[{ transform: [{ translateX: shakeAnimationEmail }] }, styles.inputWrapper]}>
+                    <TextInput
+                        placeholder="seuemail@exemplo.com"
+                        value={email}
+                        onChangeText={setEmail}
+                        style={[styles.input, errors.email && styles.inputError]}
+                        keyboardType="email-address"
+                        autoCapitalize="none"
+                    />
+                </Animated.View>
+
+                <View style={styles.labelContainer}>
+                    <Text style={styles.label}>Senha</Text>
+                    {errors.password && <Text style={styles.errorText}> (mínimo 8 caracteres, não apenas números)</Text>}
+                </View>
+                <Animated.View style={[{ transform: [{ translateX: shakeAnimationPassword }] }, styles.passwordContainer]}>
+                    <TextInput
+                        placeholder="Crie uma senha forte"
+                        value={password}
+                        onChangeText={setPassword}
+                        style={[styles.input, errors.password && styles.inputError]}
+                        secureTextEntry={!showPassword}
+                    />
+                    <TouchableOpacity onPress={() => setShowPassword(!showPassword)} style={styles.showButton}>
+                        <Ionicons name={!showPassword ? 'eye-outline' : 'eye-off-outline'} size={24} color={'#256D5B'}/>
+                    </TouchableOpacity>
+                </Animated.View>
+
+                <View style={styles.labelContainer}>
+                    <Text style={styles.label}>Confirme sua senha</Text>
+                    {errors.confirmPassword && <Text style={styles.errorText}> (as senhas não correspondem)</Text>}
+                </View>
+                <Animated.View style={[{ transform: [{ translateX: shakeAnimationConfirmPassword }] }, styles.passwordContainer]}>
+                    <TextInput
+                        placeholder="Repita a senha"
+                        value={confirmPassword}
+                        onChangeText={setConfirmPassword}
+                        style={[styles.input, errors.confirmPassword && styles.inputError]}
+                        secureTextEntry={!showConfirmPassword}
+                    />
+                    <TouchableOpacity onPress={() => setShowConfirmPassword(!showConfirmPassword)} style={styles.showButton}>
+                        <Ionicons name={!showConfirmPassword ? 'eye-outline' : 'eye-off-outline'} size={24} color={'#256D5B'}/>
+                    </TouchableOpacity>
+                </Animated.View>
+
+                <TouchableOpacity onPress={handleRegister} disabled={loading} style={styles.customButton}>
+                    {loading ? (
+                        <ActivityIndicator color="#fff" />
+                    ) : (
+                        <Text style={styles.buttonText}>Criar conta</Text>
+                    )}
+                </TouchableOpacity>
+
+                <View style={styles.loginContainer}>
+                    <Text style={styles.loginText}>Já tem uma conta?</Text>
+                    <TouchableOpacity onPress={() => navigation.replace('Login')} disabled={loading}>
+                        <Text style={styles.loginButtonText}>Faça o login</Text>
+                    </TouchableOpacity>
+                </View>
+            </ScrollView>
+        </KeyboardAvoidingView>
     );
 }
 
 const styles = StyleSheet.create({
-    container: { 
-        flex: 1, 
+    keyboardView: {
+        flex: 1,
+        backgroundColor: '#F5F5F5',
+    },
+    container: {
+        flexGrow: 1,
         alignItems: 'center',
-        justifyContent: 'center', 
-        padding: 16,
+        justifyContent: 'center',
+        paddingHorizontal: 24,
+        paddingBottom: 40,
+    },
+    title: {
+        fontSize: 28,
+        fontWeight: 'bold',
+        color: '#333',
+        marginBottom: 30,
+        textAlign: 'center',
     },
     labelContainer: {
-        alignSelf: 'flex-start',
-        marginLeft: '55'
+        width: '90%',
+        flexDirection: 'row',
+        alignItems: 'flex-end',
+        marginBottom: 5,
+    },
+    label: {
+        fontWeight: 'bold',
+        color: '#333',
+    },
+    errorText: {
+        color: 'red',
+        fontSize: 12,
+        marginLeft: 4,
+    },
+    inputWrapper: {
+        width: '90%',
     },
     input: {
-        width: '75%',
+        width: '100%',
         borderWidth: 1,
-        borderColor: '#888',
-        marginBottom: 12,
-        padding: 10,
-        borderRadius: 5,
+        backgroundColor: '#FFF',
+        borderColor: '#CCC',
+        paddingHorizontal: 15,
+        paddingVertical: 12,
+        borderRadius: 8,
+        fontSize: 16,
+        marginBottom: 16,
     },
     inputError: {
-        borderColor: 'red',
-        color: 'red'
+        borderColor: '#E53E3E',
     },
     passwordContainer: {
-        width: '75%',
+        width: '90%',
         flexDirection: 'row',
         alignItems: 'center',
-        marginBottom: 12,
-    },
-    passwordInput: {
-        flex: 1,
     },
     showButton: {
-        paddingHorizontal: 12,
-        marginBottom: 10,
-        paddingVertical: 10,
-    },
-    showButtonText: {
-        color: '#007BFF',
-        fontWeight: 'bold',
+        position: 'absolute',
+        right: 15,
+        top: 13,
+        height: 30,
     },
     customButton: {
-        width: '50%',
-        backgroundColor: '#4CAF50',
-        padding: 12,
+        width: '90%',
+        backgroundColor: '#256D5B',
+        padding: 15,
         borderRadius: 8,
         alignItems: 'center',
-        marginVertical: 8,
+        marginVertical: 16,
+        elevation: 3,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 2,
     },
     buttonText: {
         color: '#fff',
         fontWeight: 'bold',
+        fontSize: 16,
+    },
+    loginContainer: {
+        flexDirection: 'row',
+        marginTop: 20,
+    },
+    loginText: {
+        color: '#666',
+    },
+    loginButtonText: {
+        color: '#256D5B',
+        fontWeight: 'bold',
+        marginLeft: 5,
     },
 });
